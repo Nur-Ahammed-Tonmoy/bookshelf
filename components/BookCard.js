51196343fa32050c91069665ad76cf3498f2bc4a
Nur-Ahammed-Tonmoy <nur.ahamed.tonmoy@gmail.com>
2026-05-03 22:14:49 +0600

    commit

--- components/BookCard.js
+++ components/BookCard.js
@@ -4,9 +4,8 @@ import { FiStar, FiEye } from "react-icons/fi";
 
 function CategoryBadge({ category }) {
   const map = {
-    Story: "bg-purple-100 text-purple-800",
-    Tech: "bg-blue-100 text-blue-800",
     
+    Science: "bg-green-100 text-green-800",
   };
   return (
     <span
